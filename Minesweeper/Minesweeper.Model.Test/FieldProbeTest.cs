1f407061ff793f71bd7e35bad8ccae534bd0565f
Aartjes <arie.don@live.nl>
2020-08-17 21:42:11 +0200

    Add FieldProbe testcases for 3x3 field with a mine in the 0,0 coordinate

--- Minesweeper/Minesweeper.Model.Test/FieldProbeTest.cs
+++ Minesweeper/Minesweeper.Model.Test/FieldProbeTest.cs
@@ -18,10 +18,35 @@ namespace Com.Github.Aartjes.Minesweeper.Model.Test
 
         public static IEnumerable<TestCaseData> TestProbeResultCases()
         {
-            var data = new TestCaseData(new[,] { { false } }, 0, 0, FieldProbeResult.Zero);
-            yield return data;
-            data = new TestCaseData(new[,] { { true } }, 0, 0, SpaceState.Mine);
-            yield return data;
+            yield return new TestCaseData(new[,] { { false } }, 0, 0, FieldProbeResult.Zero)
+                .SetName("TestProbeResult_BlankField_0_0_Returns_Zero");
+            yield return new TestCaseData(new[,] { { true } }, 0, 0, FieldProbeResult.Mine)
+                .SetName("TestProbeResult_JustMine_0_0_Returns_Mine");
+            
+            var field = new[,]
+            {
+                { true, false, false},
+                { false, false, false},
+                { false, false, false},
+            };
+            yield return new TestCaseData(field, 0, 0, FieldProbeResult.Mine)
+                .SetName("TestProbeResult_3x3TopLeft_0_0_Returns_Mine");
+            yield return new TestCaseData(field, 1, 0, FieldProbeResult.One)
+                .SetName("TestProbeResult_3x3TopLeft_1_0_Returns_One");
+            yield return new TestCaseData(field, 0, 1, FieldProbeResult.One)
+                .SetName("TestProbeResult_3x3TopLeft_0_1_Returns_One");
+            yield return new TestCaseData(field, 1, 1, FieldProbeResult.One)
+                .SetName("TestProbeResult_3x3TopLeft_1_1_Returns_One");
+            yield return new TestCaseData(field, 2, 0, FieldProbeResult.Zero)
+                .SetName("TestProbeResult_3x3TopLeft_2_0_Returns_Zero");
+            yield return new TestCaseData(field, 2, 1, FieldProbeResult.Zero)
+                .SetName("TestProbeResult_3x3TopLeft_2_1_Returns_Zero");
+            yield return new TestCaseData(field, 2, 2, FieldProbeResult.Zero)
+                .SetName("TestProbeResult_3x3TopLeft_2_2_Returns_Zero");
+            yield return new TestCaseData(field, 1, 2, FieldProbeResult.Zero)
+                .SetName("TestProbeResult_3x3TopLeft_1_2_Returns_Zero");
+            yield return new TestCaseData(field, 0, 2, FieldProbeResult.Zero)
+                .SetName("TestProbeResult_3x3TopLeft_0_2_Returns_Zero");
         }
 
         [TestCaseSource("TestProbeResultCases")]
