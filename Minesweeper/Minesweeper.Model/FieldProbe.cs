1debec47e016ef2bdbf7d66284d269f961c1d58d
Aartjes <arie.don@live.nl>
2020-08-17 21:52:04 +0200

    Refactor "exists and has mine" logic into method to avoid too many duplicate >0 checks.

--- Minesweeper/Minesweeper.Model/FieldProbe.cs
+++ Minesweeper/Minesweeper.Model/FieldProbe.cs
@@ -4,23 +4,28 @@
     {
         public FieldProbeResult Probe(bool[,] field, int x, int y)
         {
-            if(field[x,y])
+            if (field[x, y])
             {
                 return FieldProbeResult.Mine;
             }
-            else if(x>=1 && field[x-1,y])
+            else if (SpaceExistsAndHasMine(field, x - 1, y))
             {
                 return FieldProbeResult.One;
             }
-            else if (y >= 1 && field[x, y -1])
+            else if (SpaceExistsAndHasMine(field, x, y - 1))
             {
                 return FieldProbeResult.One;
             }
-            else if (x >= 1 && y >= 1 && field[x -1, y - 1])
+            else if (SpaceExistsAndHasMine(field, x - 1, y - 1))
             {
                 return FieldProbeResult.One;
             }
             return FieldProbeResult.Zero;
+        }
+
+        private bool SpaceExistsAndHasMine(bool[,] field, int x, int y)
+        {
+            return x >= 0 && y >= 0 && field[x, y];
         }
     }
 }
