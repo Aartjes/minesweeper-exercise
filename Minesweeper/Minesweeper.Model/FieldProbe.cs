bd94b5eb8a05e2a7e8aec0053cfa0fa69d631a02
Aartjes <arie.don@live.nl>
2020-08-17 21:05:47 +0200

    Add first FieldProbe testcase

---
+++ Minesweeper/Minesweeper.Model/FieldProbe.cs
@@ -0,0 +1,10 @@
+﻿namespace Com.Github.Aartjes.Minesweeper.Model
+{
+    public class FieldProbe
+    {
+        public FieldProbeResult Probe(bool[,] field, int x, int y)
+        {
+            return FieldProbeResult.Zero;
+        }
+    }
+}
