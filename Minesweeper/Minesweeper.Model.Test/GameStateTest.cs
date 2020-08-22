645c7950f2d8bd8c31f380f86f9c3fbdf85f034a
Aartjes <arie.don@live.nl>
2020-08-22 22:08:31 +0200

    Begin to add GameState starting with all spaces blank.

---
+++ Minesweeper/Minesweeper.Model.Test/GameStateTest.cs
@@ -0,0 +1,66 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Github.Aartjes.Minesweeper.Model.Test
+{
+    [TestFixture]
+    public class GameStateTest
+    {
+        private Field _field;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var fieldArray = new bool[10, 10];
+            fieldArray[0, 0] = true;
+            fieldArray[1, 1] = true;
+            fieldArray[2, 2] = true;
+            fieldArray[4, 2] = true;
+            fieldArray[3, 3] = true;
+            fieldArray[5, 3] = true;
+            fieldArray[6, 3] = true;
+            fieldArray[4, 4] = true;
+            fieldArray[6, 4] = true;
+            fieldArray[7, 4] = true;
+            fieldArray[5, 5] = true;
+            fieldArray[7, 5] = true;
+            fieldArray[8, 5] = true;
+            fieldArray[5, 6] = true;
+            fieldArray[6, 6] = true;
+            fieldArray[8, 6] = true;
+            fieldArray[6, 7] = true;
+            fieldArray[7, 7] = true;
+            fieldArray[8, 7] = true;
+            fieldArray[9, 7] = true;
+            fieldArray[7, 8] = true;
+            fieldArray[9, 8] = true;
+            fieldArray[7, 9] = true;
+            fieldArray[8, 9] = true;
+            fieldArray[9, 9] = true;
+            _field = new Field(fieldArray);
+        }
+
+        [Test]
+        public void GameStateStartsWithAllSpacesBlank()
+        {
+            var gameState = new GameState(_field);
+            Assert.AreEqual(10, gameState.FieldWidth);
+            Assert.AreEqual(10, gameState.FieldHeight);
+
+            for (int x = 0; x < gameState.FieldWidth; x++)
+            {
+                for (int y = 0; y < gameState.FieldHeight; y++)
+                {
+                    if (gameState[x, y] != GameSpaceState.Blank)
+                    {
+                        Assert.Fail();
+                    }
+                }
+            }
+            Assert.Pass();
+        }
+    }
+}
---
+++ Minesweeper/Minesweeper.Model/GameState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Github.Aartjes.Minesweeper.Model
+{
+    public class GameState
+    {
+        private Field _field;
+
+        public GameState(Field field)
+        {
+            _field = field;
+        }
+
+        public GameSpaceState this[int x, int y] => GameSpaceState.Blank;
+
+        public int FieldWidth => _field.Width;
+        public int FieldHeight => _field.Height;
+    }
+}
