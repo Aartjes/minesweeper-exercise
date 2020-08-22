551f7f51c158c081cd0fae0dc1b87ac230e71f28
Aartjes <arie.don@live.nl>
2020-08-22 15:31:32 +0200

    Add fieldclass that manages the dimensions of the 2d field array for some ease of use.

---
+++ Minesweeper/Minesweeper.Model/Field.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace Com.Github.Aartjes.Minesweeper.Model
+{
+    public class Field
+    {
+        private bool[,] _fieldArray;
+
+        public Field(bool[,] fieldArray)
+        {
+            _fieldArray = fieldArray;
+        }
+
+        public int Width => _fieldArray.GetLength(0);
+        public int Height => _fieldArray.GetLength(1);
+        public int MineCount => _fieldArray.Cast<bool>().Count(value => value);
+    }
+}
