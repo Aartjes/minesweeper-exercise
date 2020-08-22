9a6257fdae783e87230b8dcc3f445992e1989778
Aartjes <arie.don@live.nl>
2020-08-22 22:08:31 +0200

    Begin to add GameState starting with all spaces blank.

---
+++ Minesweeper/Minesweeper.Model/GameSpaceState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Github.Aartjes.Minesweeper.Model
+{
+    public enum GameSpaceState
+    {
+        Mine  = -1,
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Blank = 9
+    }
+}
