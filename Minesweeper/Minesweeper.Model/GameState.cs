1860b2e3c53362e3a63790afdf281d3d53bf87bb
Aartjes <arie.don@live.nl>
2020-08-22 22:47:54 +0200

    Store GameState per space

--- Minesweeper/Minesweeper.Model/GameState.cs
+++ Minesweeper/Minesweeper.Model/GameState.cs
@@ -9,36 +9,44 @@ namespace Com.Github.Aartjes.Minesweeper.Model
         private Field _field;
         private readonly IFieldProbe _probe;
         private readonly ISpaceStateConvertor _spaceStateConverter;
-        private GameSpaceState _state = GameSpaceState.Blank;
+        private GameSpaceState[,] _state;
 
         public GameState(Field field, IFieldProbe probe, ISpaceStateConvertor spaceStateConverter)
         {
             _field = field;
             _probe = probe;
             _spaceStateConverter = spaceStateConverter;
+            _state = new GameSpaceState[FieldWidth, FieldHeight];
+            for (int x = 0; x < FieldWidth; x++)
+            {
+                for (int y = 0; y < FieldHeight; y++)
+                {
+                    _state[x, y] = GameSpaceState.Blank;
+                }
+            }
         }
 
-        public GameSpaceState this[int x, int y] => _state;
+        public GameSpaceState this[int x, int y] => _state[x, y];
 
         public int FieldWidth => _field.Width;
         public int FieldHeight => _field.Height;
 
         public void ToggleFlag(int x, int y)
         {
-            switch(_state)
+            switch (_state[x, y])
             {
                 case GameSpaceState.Blank:
-                    _state = GameSpaceState.Flag;
+                    _state[x, y] = GameSpaceState.Flag;
                     break;
                 case GameSpaceState.Flag:
-                    _state = GameSpaceState.Blank;
+                    _state[x, y] = GameSpaceState.Blank;
                     break;
             }
         }
 
         public void Step(int x, int y)
         {
-            _state = _spaceStateConverter.Convert(_probe.Probe(_field, x, y));
+            _state[x, y] = _spaceStateConverter.Convert(_probe.Probe(_field, x, y));
         }
     }
 }
