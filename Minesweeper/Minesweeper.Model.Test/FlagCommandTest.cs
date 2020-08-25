c8cba5e780df501bb13060f354bd96420dd7be49
Aartjes <arie.don@live.nl>
2020-08-25 23:29:09 +0200

    Add Flag command

---
+++ Minesweeper/Minesweeper.Model.Test/FlagCommandTest.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Github.Aartjes.Minesweeper.Model.Test
+{
+    [TestFixture]
+    public class FlagCommandTest
+    {
+        [Test]
+        public void Execute_RunsToggleFlagOnFieldAtGivenSpace()
+        {
+            bool testSuccesful = false;
+            var gameStateMock = new Mock<IGameState>();
+            gameStateMock.Setup(
+                state => state.ToggleFlag(
+                    It.Is<int>(x => x == 12),
+                    It.Is<int>(y => y == 13)))
+                .Callback(() => testSuccesful = true);
+            new FlagCommand(12, 13)
+                .Execute(gameStateMock.Object);
+
+
+            Assert.IsTrue(testSuccesful);
+        }
+    }
+}
---
+++ Minesweeper/Minesweeper.Model/FlagCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Github.Aartjes.Minesweeper.Model
+{
+    public class FlagCommand
+    {
+        private readonly int _x;
+        private readonly int _y;
+
+        public FlagCommand(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public void Execute(IGameState gameState)
+        {
+            gameState.ToggleFlag(_x, _y);
+        }
+    }
+}
