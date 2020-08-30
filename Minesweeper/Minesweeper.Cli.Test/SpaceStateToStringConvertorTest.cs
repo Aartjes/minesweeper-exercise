ad4c3106f8858e6609feaf7d6bbd33f93a54a3f4
Aartjes <arie.don@live.nl>
2020-08-30 18:31:31 +0200

    Add logic to convert a GameSpaceState to a string.

---
+++ Minesweeper/Minesweeper.Cli.Test/SpaceStateToStringConvertorTest.cs
@@ -0,0 +1,28 @@
+﻿using Com.Github.Aartjes.Minesweeper.Cli;
+using Com.Github.Aartjes.Minesweeper.Model;
+using NUnit.Framework;
+
+namespace Com.Gitlab.Aartjes.Minesweeper.Cli.Test
+{
+    public class SpaceStateToStringConvertorTest
+    {
+        [TestCase(GameSpaceState.Blank, ".")]
+        [TestCase(GameSpaceState.Flag,  "P")]
+        [TestCase(GameSpaceState.Mine,  "*")]
+        [TestCase(GameSpaceState.Zero,  "0")]
+        [TestCase(GameSpaceState.One,   "1")]
+        [TestCase(GameSpaceState.Two,   "2")]
+        [TestCase(GameSpaceState.Three, "3")]
+        [TestCase(GameSpaceState.Four,  "4")]
+        [TestCase(GameSpaceState.Five,  "5")]
+        [TestCase(GameSpaceState.Six,   "6")]
+        [TestCase(GameSpaceState.Seven, "7")]
+        [TestCase(GameSpaceState.Eight, "8")]
+        public void TestSpaceStateToStringConversion(GameSpaceState input, string expectedOutput)
+        {
+            var convertor = new SpaceStateToStringConvertor();
+            var output = convertor.Convert(input);
+            Assert.AreEqual(expectedOutput, output);
+        }
+    }
+}
---
+++ Minesweeper/Minesweeper.Cli/SpaceStateToStringConvertor.cs
@@ -0,0 +1,28 @@
+﻿using Com.Github.Aartjes.Minesweeper.Model;
+using System;
+
+namespace Com.Github.Aartjes.Minesweeper.Cli
+{
+    public class SpaceStateToStringConvertor
+    {
+        public string Convert(GameSpaceState input)
+        {
+            return input switch
+            {
+                GameSpaceState.Blank => ".",
+                GameSpaceState.Flag => "P",
+                GameSpaceState.Mine => "*",
+                GameSpaceState.Zero => "0",
+                GameSpaceState.One => "1",
+                GameSpaceState.Two => "2",
+                GameSpaceState.Three => "3",
+                GameSpaceState.Four => "4",
+                GameSpaceState.Five => "5",
+                GameSpaceState.Six => "6",
+                GameSpaceState.Seven => "7",
+                GameSpaceState.Eight => "8",
+                _ => throw new NotSupportedException(),
+            };
+        }
+    }
+}
