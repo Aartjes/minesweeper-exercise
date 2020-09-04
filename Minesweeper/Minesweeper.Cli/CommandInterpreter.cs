4d543b801e16c4c7cd115a9610abac750b5bcec8
Aartjes <arie.don@live.nl>
2020-09-04 21:26:47 +0200

    Refactor duplicate gamecommand interpretation logic to separate function.

--- Minesweeper/Minesweeper.Cli/CommandInterpreter.cs
+++ Minesweeper/Minesweeper.Cli/CommandInterpreter.cs
@@ -20,31 +20,41 @@ namespace Com.Github.Aartjes.Minesweeper.Cli
             var parts = command.Split(" \t,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
             if (string.Equals("step",parts[0],StringComparison.CurrentCultureIgnoreCase))
             {
-                var state = _program.Game.ExecuteCommand(
-                    new StepCommand(
-                        int.Parse(parts[1], CultureInfo.CurrentCulture) -1,
-                        int.Parse(parts[2], CultureInfo.CurrentCulture) -1));
-                switch (state)
-                {
-                    case GameStatus.Loss:
-                        _program.Lose();
-                        break;
-                    case GameStatus.Win:
-                        _program.Win();
-                        break;
-                }
+                CreateAndExecuteGameCommand((x, y) => new StepCommand(x, y), parts[1], parts[2]);
             }
             else if (string.Equals("flag", parts[0], StringComparison.CurrentCultureIgnoreCase))
             {
-                _program.Game.ExecuteCommand(
-                    new FlagCommand(
-                        int.Parse(parts[1], CultureInfo.CurrentCulture) -1,
-                        int.Parse(parts[2], CultureInfo.CurrentCulture) -1));
+                CreateAndExecuteGameCommand((x, y) => new FlagCommand(x, y), parts[1], parts[2]);
             }
             else if (string.Equals("exit", parts[0], StringComparison.CurrentCultureIgnoreCase))
             {
                 _program.Exit();
             }
+        }
+
+        private void CreateAndExecuteGameCommand(Func<int, int, IGameCommand> createCommand, string xString, string yString)
+        {
+            var command = createCommand(
+                ToCoordinate(xString),
+                ToCoordinate(yString));
+            var state = _program.Game.ExecuteCommand(command);
+            switch (state)
+            {
+                case GameStatus.Loss:
+                    _program.Lose();
+                    break;
+                case GameStatus.Win:
+                    _program.Win();
+                    break;
+                case GameStatus.Ongoing:
+                default:
+                    break;
+            }
+        }
+
+        private int ToCoordinate(string coordinateString)
+        {
+            return int.Parse(coordinateString, CultureInfo.CurrentCulture) - 1;
         }
     }
 }
