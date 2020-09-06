b47c8d5cd628bb60c8a8bd24adb2530fb32c41f8
Aartjes <arie.don@live.nl>
2020-09-06 22:03:37 +0200

    Added TextResources implementation, and a wrapper for the console logic. Note that this can not be unit tested because they directly use the Console, and the resources.

---
+++ Minesweeper/Minesweeper.Cli/ConsoleWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Github.Aartjes.Minesweeper.Cli
+{
+    class ConsoleWrapper : IConsole
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public void WriteLine(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ Minesweeper/Minesweeper.Cli/TextResources.cs
@@ -0,0 +1,18 @@
+﻿using Com.Github.Aartjes.Minesweeper.Cli.Properties;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Github.Aartjes.Minesweeper.Cli
+{
+    public class TextResources : ITextResources
+    {
+        public string CommandQuery => Resources.CommandQuery;
+
+        public string NewGameQuery => Resources.NewGameQuery;
+
+        public string LossMessage => Resources.LossMessage;
+
+        public string VictoryMessage => VictoryMessage;
+    }
+}
