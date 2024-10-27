ebfa4e20681914be23edbb1c9f768a731a77908a
Aartjes <arie.don@live.nl>
2024-10-27 10:50:30 +0100

    Create a solution and a dummy CLI application.

---
+++ Minesweeper/Minesweeper.Cli/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Work in progress. Eventually, running this project will allow the user to play minesweeper in the CLI");
